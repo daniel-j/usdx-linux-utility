cb6c2e5798b8196b4fd9081a8c63522f8de3c296
daniel-j <daniel.j.hede@gmail.com>
2017-02-17 23:06:18 +0100

    Support multiple txt files in same folder

--- mlk-open.sh
+++ mlk-open.sh
@@ -1,48 +1,96 @@
 #!/bin/bash
 
 input="$1"
+simplename="$(basename "$input")"
 
-tmp=$(mktemp -d)
-#tar -xf "$input" -C "$tmp"
+if [ ! -e "$input" ]; then
+	zenity --error --text="File does not exist\n$input"
+	exit 1
+fi
 
-files=$(tar -tf "$input" | sort | grep -F "notes.txt")
+songs=$(tar -tf "$input" | sort | grep -i "\.txt$")
 
-#escapehtml() {
-#	sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' <<< "$1"
-#}
-#eval "echo \"$(< ./prompt.html)\"" | zenity --text-info --title="Add song" --filename=/dev/stdin --html --width=600 --height=300
+if [[ $? -ne 0 ]]; then
+	zenity --error --text="Could not read tar archive\n$input"
+	exit 1
+fi
 
-declare -a data
+songcount=$(wc -l <<< "$songs")
 
-while read -r notespath
+if [[ $songcount -lt 1 ]]; then
+	zenity --error --text="No songs found"
+	exit 1
+fi
+
+escapehtml() {
+	sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' <<< "$1"
+}
+
+add_one() {
+	songpath="$songs"
+	list="$songpath"
+	load_song "$songpath"
+	if [ ! -z "$cover" ]; then
+		coverpath="$directory/$cover"
+		image="data:image;base64,$(tar -xf "$input" "$coverpath" -O | base64 -w 0)"
+	fi
+
+	title=$(escapehtml "$title")
+	artist=$(escapehtml "$artist")
+	creator=$(escapehtml "$creator")
+	language=$(escapehtml "$language")
+	songpath=$(escapehtml "$songs")
+	eval "echo \"$(< ./prompt.html)\"" | zenity --text-info --title="Add song from \"$simplename\"" --filename=/dev/stdin --html --width=850 --height=350
+}
+
+add_many() {
+	declare -a data
+
+	while read -r songpath
+	do
+		load_song "$songpath"
+
+		data+=("TRUE" "$songpath" "$title" "$artist" "$creator" "$language")
+	done <<< "$songs"
+	list=$(zenity --list --checklist --title="Add songs from \"$simplename\"" --width=800 --height=350 --separator='\n' --column="" --column="Song file" --column="Title" --column="Artist" --column="Creator" --column="Language" "${data[@]}")
+}
+
+load_song() {
+	song=$(tar -xf "$input" "$1" -O)
+	directory=$(dirname "$1")
+	title=$(echo "$song" | grep "^#TITLE:" | cut -c 8- | tr -d '\r\n')
+	artist=$(echo "$song" | grep "^#ARTIST:" | cut -c 9- | tr -d '\r\n')
+	creator=$(echo "$song" | grep "^#CREATOR:" | cut -c 10- | tr -d '\r\n')
+	language=$(echo "$song" | grep "^#LANGUAGE:" | cut -c 11- | tr -d '\r\n')
+	cover=$(echo "$song" | grep "^#COVER:" | cut -c 8- | tr -d '\r\n')
+	audio=$(echo "$song" | grep "^#MP3:" | cut -c 6- | tr -d '\r\n')
+	video=$(echo "$song" | grep "^#VIDEO:" | cut -c 8- | tr -d '\r\n')
+	background=$(echo "$song" | grep "^#BACKGROUND:" | cut -c 13- | tr -d '\r\n')
+}
+
+if [[ $songcount -eq 1 ]]; then
+	add_one
+else
+	add_many
+fi
+
+if [[ $? -ne 0 ]] || [ -z "$list" ]; then exit 0; fi
+
+outdir="$HOME/.ultrastardx/songs"
+
+while read -r songpath
 do
-	notes=$(tar -xf "$input" "$notespath" -O)
-	title=$(echo "$notes" | grep "^#TITLE:" | cut -c 8- | tr -d '\r\n')
-	artist=$(echo "$notes" | grep "^#ARTIST:" | cut -c 9- | tr -d '\r\n')
-	creator=$(echo "$notes" | grep "^#CREATOR:" | cut -c 10- | tr -d '\r\n')
-	language=$(echo "$notes" | grep "^#LANGUAGE:" | cut -c 11- | tr -d '\r\n')
-	directory="$(dirname "$notespath")"
-
-	data+=("TRUE" "$directory" "$title" "$artist" "$creator" "$language")
-done <<< "$files"
-
-list=$(zenity --list --checklist --title="Add songs to My Little Karaoke from \"$(basename "$input" ".tar.mlk")\"" --width=800 --height=350 --separator='\n' --column="" --column="Directory" --column="Title" --column="Artist" --column="Creator" --column="Language" "${data[@]}")
-
-case $? in
-	1)
-		exit 0
-		;;
-	-1)
-		echo "An unexpected error has occurred."
-		exit 1
-		;;
-esac
-
-outdir="$HOME/.ultrastardx/songs/"
-
-while read -r directory
-do
+	load_song "$songpath"
+	if [ -e "$outdir/$songpath" ]; then
+		zenity --question --text="This song already exists. Do you want to overwrite it?\n\n$artist - $title\n\n$outdir/$songpath" --title="Overwrite song?"
+		if [[ $? -ne 0 ]]; then continue; fi
+	fi
 	mkdir -pv "$outdir"
-	tar -xvf "$input" -C "$outdir" "$directory"
+	echo "Extracting song to $outdir/$songpath"
+	files=("$songpath")
+	if [ ! -z "$cover" ]; then files+=("$directory/$cover"); fi
+	if [ ! -z "$audio" ]; then files+=("$directory/$audio"); fi
+	if [ ! -z "$video" ]; then files+=("$directory/$video"); fi
+	if [ ! -z "$background" ]; then files+=("$directory/$background"); fi
+	tar -xvf "$input" -C "$outdir" "${files[@]}"
 done <<< "$list"
-
